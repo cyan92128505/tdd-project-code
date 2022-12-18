55c385ada2e9e574ec39a002163090999faa7043
cyan92128505 <cyan92128505@gmail.com>
2022-12-18 13:17:17 +0800

    refactor: improved test

--- dart/test/money_test.dart
+++ dart/test/money_test.dart
@@ -3,28 +3,21 @@ import 'package:money/portfolio.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('5 x 2 = 10', () {
+  test('Test Multiplication: 5 x 2 = 10', () {
     var actual = Money(5, 'USD');
     var expected = Money(10, 'USD');
 
     expect(expected, actual.times(2));
   });
 
-  test('10 EUR x 2 = 20 EUR', () {
-    var actual = Money(10, 'EUR');
-    var expected = Money(20, 'EUR');
-
-    expect(expected, actual.times(2));
-  });
-
-  test('4002 KRW / 4 = 1000.5 KRW', () {
+  test('Test Division: 4002 KRW / 4 = 1000.5 KRW', () {
     var actual = Money(4002, 'KRW');
     var expected = Money(1000.5, 'KRW');
 
     expect(expected, actual.divide(4));
   });
 
-  test('10 USD + 5 USD = 15 USD', () {
+  test('Test Addtion: 10 USD + 5 USD = 15 USD', () {
     var fiveDollars = Money(5, "USD");
     var tenDollars = Money(10, "USD");
     var fifteenDollars = Money(15, "USD");
